a805353dcacaf30ba37e8495c19a38ae04b4e67b
lidong1998 <729790146@qq.com>
2020-01-02 15:57:12 +0800

    Finish lab3

--- lab3/Flik/HorribleSteve.java
+++ lab3/Flik/HorribleSteve.java
@@ -1,10 +1,16 @@
+import static org.junit.Assert.*;
+import org.junit.Test.*;
+import org.junit.runners.JUnit4;
+
 public class HorribleSteve {
     public static void main(String [] args) throws Exception {
         int i = 0;
         for (int j = 0; i < 500; ++i, ++j) {
             if (!Flik.isSameNumber(i, j)) {
-                throw new Exception(
-                        String.format("i:%d not same as j:%d ??", i, j));
+//                throw new Exception(
+//                        String.format("i:%d not same as j:%d ??", i, j));
+                System.out.println("i = " + i + ", j = " + j);
+                assertTrue(i == j);
             }
         }
         System.out.println("i is " + i);
