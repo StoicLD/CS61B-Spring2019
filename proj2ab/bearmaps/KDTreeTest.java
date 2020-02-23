4ed473bb5c5ff91f1b881566525cf5f6c65f8ad0
Lidong <729790146@qq.com>
2020-02-23 12:43:59 +0800

    I think I finished Project2b

--- proj2ab/bearmaps/KDTreeTest.java
+++ proj2ab/bearmaps/KDTreeTest.java
@@ -2,7 +2,9 @@ package bearmaps;
 
 import org.junit.Test;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
 
@@ -41,5 +43,30 @@ public class KDTreeTest
         KDTree kdTree = new KDTree(list);
         assertEquals(new Point(1, 5), kdTree.nearest(0, 7));
         System.out.println(KDTree.getTimes());
+    }
+
+    @Test
+    public void nearestTest2()
+    {
+        Random rng = new Random(); // Ideally just create one instance globally
+        final double bound = 1000;
+        //随机测试
+        LinkedList<Point> list = new LinkedList<>();
+        int totalNum = 10000;
+        for(int i = 0; i < totalNum; i++)
+        {
+            list.add(new Point(rng.nextDouble() * bound, rng.nextDouble() * bound));
+        }
+
+        NaivePointSet nps = new NaivePointSet(list);
+        KDTree kdt = new KDTree(list);
+
+        System.out.println("NaviePointSet calling time is : " + totalNum);
+        for(int i = 0; i < 10; i++)
+        {
+            Point targetPoint = new Point(rng.nextDouble() * bound, rng.nextDouble() * bound);
+            assertEquals(nps.nearest(targetPoint.getX(), targetPoint.getY()), kdt.nearest(targetPoint.getX(), targetPoint.getY()));
+            System.out.println("KdTree calling time is : " + KDTree.getTimes());
+        }
     }
 }
